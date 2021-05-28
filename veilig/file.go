4bf7349cd8668ec0b18e3c368f9205ed0ce9b410
Florian Baumann <flo@noqqe.de>
2021-05-28 10:05:35 +0200

    Add misc infos for file certs

--- veilig/file.go
+++ veilig/file.go
@@ -5,14 +5,19 @@ import (
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 // Source: https://gist.github.com/ukautz/cd118e298bbd8f0a88fc
 func LoadCertificateFromFile(path string) ([]*x509.Certificate, error) {
+
 	raw, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
+
+	fi, _ := os.Stat(path)
+	size := fi.Size()
 
 	var chain []*x509.Certificate
 	var cert *x509.Certificate
@@ -24,7 +29,6 @@ func LoadCertificateFromFile(path string) ([]*x509.Certificate, error) {
 		if block.Type == "CERTIFICATE" {
 			cert, err = x509.ParseCertificate(block.Bytes)
 			chain = append(chain, cert)
-			break
 		}
 		raw = rest
 	}
@@ -32,6 +36,8 @@ func LoadCertificateFromFile(path string) ([]*x509.Certificate, error) {
 	if len(chain) == 0 {
 		return nil, fmt.Errorf("No certificate found in \"%s\"", path)
 	}
+
+	fmt.Printf("%sFile: %s (%d bytes) with %d certificate(s)%s\n\n", Comment, path, size, len(chain), Reset)
 
 	return chain, nil
 }
