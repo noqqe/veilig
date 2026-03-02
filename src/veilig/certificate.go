796a084a5c529170d93c57a09e22c5a780dfea37
Florian Baumann <flo@noqqe.de>
2026-03-02 15:37:49 +0100

    Switch to sprintf functions of color lib

--- src/veilig/certificate.go
+++ src/veilig/certificate.go
@@ -4,43 +4,54 @@ import (
 	"crypto/ecdsa"
 	"crypto/rsa"
 	"crypto/x509"
+	"fmt"
 	"strings"
 
 	"github.com/fatih/color"
 )
 
+var (
+	Cyan    = color.New(color.FgCyan).SprintFunc()
+	Green   = color.New(color.FgGreen).SprintFunc()
+	Red     = color.New(color.FgRed).SprintFunc()
+	Yellow  = color.New(color.FgYellow).SprintFunc()
+	Magenta = color.New(color.FgMagenta).SprintFunc()
+	Pink    = color.New(color.FgHiMagenta).SprintFunc()
+	Black   = color.New(color.FgHiBlack).SprintFunc()
+)
+
 // Takes certificate struct and prints values
 func printCertificate(cert *x509.Certificate) bool {
 
-	color.Green("Subject:\t%s\n", cert.Subject)
-	color.Yellow("Valid from:\t%s\n", cert.NotBefore)
-	color.Yellow("Valid until:\t%s\n", cert.NotAfter)
+	fmt.Printf("Subject:\t%s\n", Green(cert.Subject))
+	fmt.Printf("Valid from:\t%s\n", Yellow(cert.NotBefore))
+	fmt.Printf("Valid until:\t%s\n", Yellow(cert.NotAfter))
 
 	if len(cert.Issuer.Organization) > 0 {
-		color.Cyan("Issuer:\t\t%s\n", cert.Issuer.Organization[0])
+		fmt.Printf("Issuer:\t\t%s\n", Cyan(cert.Issuer.Organization[0]))
 	}
 
-	color.HiCyan("Is CA:\t\t%t\n", cert.IsCA)
-	color.HiMagenta("Signature:\t%s\n", cert.SignatureAlgorithm)
+	fmt.Printf("Is CA:\t\t%s\n", Cyan(cert.IsCA))
+	fmt.Printf("Signature:\t%s\n", Magenta(cert.SignatureAlgorithm))
 
 	switch cert.PublicKey.(type) {
 	case *rsa.PublicKey:
 		bits := cert.PublicKey.(*rsa.PublicKey)
-		color.HiMagenta("PublicKey:\t%s (%d bits)\n", cert.PublicKeyAlgorithm, bits.Size()*8)
+		fmt.Printf("PublicKey:\t%s (%d bits)\n", Magenta(cert.PublicKeyAlgorithm), bits.Size()*8)
 	case *ecdsa.PublicKey:
 		curve := cert.PublicKey.(*ecdsa.PublicKey)
 		params := curve.Params()
-		color.HiMagenta("PublicKey:\t%s %v (%d bits)\n", cert.PublicKeyAlgorithm, params.Name, params.BitSize)
+		fmt.Printf("PublicKey:\t%s %v (%d bits)\n", Magenta(cert.PublicKeyAlgorithm), params.Name, params.BitSize)
 	default:
-		color.HiMagenta("PublicKey:%s\t%s%s\n", cert.PublicKeyAlgorithm)
+		fmt.Printf("PublicKey:%s\t%s%s\n", Magenta(cert.PublicKeyAlgorithm))
 	}
 
 	if len(cert.DNSNames) > 0 {
-		color.HiCyan("DNS Names:\t%s\n", strings.Join(cert.DNSNames, ", "))
+		fmt.Printf("DNS Names:\t%s\n", Cyan(strings.Join(cert.DNSNames, ", ")))
 	}
 
 	if len(cert.OCSPServer) > 0 {
-		color.HiBlue("OCSP Server:\t%s\n", strings.Join(cert.OCSPServer, ", "))
+		fmt.Printf("OCSP Server:\t%s\n", Cyan(strings.Join(cert.OCSPServer, ", ")))
 	}
 
 	return true
@@ -51,9 +62,9 @@ func verifyCertificate(cert *x509.Certificate, host string) {
 	if !cert.IsCA {
 		err := cert.VerifyHostname(host)
 		if err == nil {
-			color.Green("Name Valid:\t%s\n", "true")
+			fmt.Printf("Name Valid:\t%s\n", Green("true"))
 		} else {
-			color.Red("Name Valid:\t%s%s\n", "false: ", cert.VerifyHostname(host))
+			fmt.Printf("Name Valid:\t%s%s\n", "false: ", Red(cert.VerifyHostname(host)))
 		}
 	}
 }
